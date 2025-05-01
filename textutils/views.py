057f133cc3812b42772ae2773cf790a15131a190
Shibadipta Pal <shibadiptapal@gmail.com>
2025-05-01 20:57:32 +0530

    Update views.py

--- textutils/views.py
+++ textutils/views.py
@@ -17,7 +17,7 @@ def analyze(request):
     newlineremover = request.POST.get('newlineremover', 'off')
     extraspaceremover = request.POST.get('extraspaceremover', 'off')
     numberremover = request.POST.get('numberremover','off')
-
+    purpose = []
     #Check which checkbox is on
     if removepunc == "on":
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
@@ -25,16 +25,14 @@ def analyze(request):
         for char in djtext:
             if char not in punctuations:
                 analyzed = analyzed + char
-
-        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        purpose.append('Removed Punctuations')
         djtext = analyzed
 
     if(fullcaps=="on"):
         analyzed = ""
         for char in djtext:
             analyzed = analyzed + char.upper()
-
-        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        purpose.append('Changed to Uppercase')
         djtext = analyzed
 
     if(extraspaceremover=="on"):
@@ -47,8 +45,7 @@ def analyze(request):
 
             elif not(djtext[index] == " " and djtext[index+1]==" "):                        
                 analyzed = analyzed + char
-
-        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        purpose.append('Removed Extra Spaces')
         djtext = analyzed
 
     if (newlineremover == "on"):
@@ -56,8 +53,8 @@ def analyze(request):
         for char in djtext:
             if char != "\n" and char!="\r":
                 analyzed = analyzed + char
-
-        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        purpose.append('Removed NewLines')
+        djtext = analyzed
     
     if (numberremover == "on"):
         analyzed = ""
@@ -66,10 +63,8 @@ def analyze(request):
         for char in djtext:
             if char not in numbers:
                 analyzed = analyzed + char
-        
-        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-        djtext = analyzed
-
+        purpose.append('Removed Number')
+    params = {'purpose': purpose, 'analyzed_text': analyzed}
     
     if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on" and numberremover != "on"):
         return HttpResponse("please select any operation and try again")
